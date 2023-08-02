49d8c6b798fb822a802f2b16a6c00bdb44b5f30a
Andrew Kuang <127608232+drmcducks@users.noreply.github.com>
2023-08-02 21:19:33 +0700

    add files manually

---
+++ index.html
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bella And Mortimer Wedding</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="wrap">
+        <h1 class="title" onclick="window.location='index.html'">Bella & Mortimer</h1>
+        <h2>23 . 03 . 2023</h2>
+        <a href="rsvp-form.php"><button class="buttons">RSVP</button></a>
+        <a href="about.html"><button class="buttons">Our Story</button></a>
+        <a href="location.html"><button class="buttons">Location</button></a>
+    </div>
+    <footer>© 2023 Drew | <a href="login.html">Log-in</a> </footer>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bella And Mortimer Wedding</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+    if (isset($_POST["Login"])){
+        $email = $_POST["email"];
+        $passwd = $_POST["passwd"];
+        require_once "database-con.php";
+        $sql = "SELECT * FROM login WHERE email = '$email' ";
+        $result = mysqli_query($conn, $sql);
+        $login = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+    }
+    ?>
+    <div class="wrap">
+    <form id="frm-login" accept-charset="utf-8" action="" method="post">
+           <label for="email">Email</label><br>
+           <input type="text" name="email" id="email" placeholder="Input Your Email">
+           <br>
+           <label for="passwd">Password</label><br>
+           <input type="password" class="pwinput" name="passwd" id="passwd" placeholder="Input Your Password">
+           <br>
+           <input type="submit" value="Login">
+    </div>
+    <footer>© 2023 Drew | <a href="logout.php">Logout</a></footer>
+</body>
+</html>
